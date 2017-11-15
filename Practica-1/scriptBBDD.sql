d22bdf08600e4ce7ad9a17676445e1aa48d2a672
Josué Pradas <jpradas@ucm.es>
2017-11-15 10:54:26 +0100

    cambiada la jerarquia de carpetas segun el framework express y ejs

--- Practica-1/scriptBBDD.sql
+++ Practica-1/scriptBBDD.sql
@@ -2,18 +2,12 @@ CREATE DATABASE facebluff;
 USE facebluff;
 
 CREATE TABLE control_usuarios(
-	email VARCHAR(30) PRIMARY KEY NOT NULL AUTO_INCREMENT, 
+	id INT AUTO_INCREMENT,
+	email VARCHAR(30) PRIMARY KEY NOT NULL, 
     contraseña VARCHAR(30) NOT NULL,
     nombre_completo VARCHAR(50),
     sexo CHAR,
     fecha_nacimiento DATE,
-    imagen VARCHAR(20),
+    imagen VARCHAR(50),
 	puntos INT,
-    FOREIGN KEY (imagen) REFERENCES imagenes(id)
-);
-
-CREATE TABLE imagenes(
-	id VARCHAR(20) PRIMARY KEY,
-    imagen LONGBLOB,
-    tipo VARCHAR(100)
 );
