87c1e044c27dea379c37845f18e3657eb9e81c3b
Josué Pradas <jpradas@ucm.es>
2017-11-24 18:04:13 +0100

    hecho mensaje flash, falta darle estilo.

--- Practica-1/main.js
+++ Practica-1/main.js
@@ -7,6 +7,7 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const mysql = require("mysql");
 const session = require("express-session");
+const flash = require("express-flash")
 const config = require("./config");
 const daoUsers = require("./daoUsers");
 
@@ -35,7 +36,12 @@ app.set("views", path.join(__dirname, "views"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(middlewareSession);
+app.use(flash());
 
+
+function setFlash(request, msg){
+  request.session.flashMsg = msg;
+}
 
 function userExists(request, response, next){
 	daou.isUserCorrect(request.body.email, request.body.password, (err, result)=>{
@@ -45,7 +51,8 @@ function userExists(request, response, next){
 		if(result){
 			next();
 		}else{//mostrar alerta de usuario no encontrado
-			response.redirect("index.html");
+      setFlash(request, "Usuario inexistente en base de datos");
+      response.render("login", {message: request.session.flashMsg});
 		}
 	});
 }
@@ -109,14 +116,42 @@ function servUser(request, response){
 		}
 		response.status(200);
 		response.render("profile", { usuario: user });
-	})
+	});
 }
+
+
+app.get("/modify_profile", (request, response, next) =>{
+  daou.getUser(request.session.email, (err, user)=>{
+    if(err){
+      next(err);
+    }
+    response.status(200);
+    response.render("modify_profile", { usuario: user });
+  });
+
+  /*console.log("hola");
+  response.status(200);
+  response.end();*/
+})
+
+app.get("/", (request, response, next)=>{
+  response.render("login", {message: ""});
+})
 
 //manejador de error
 app.use((error, request, response, next) =>{
 	//codigo 500: Internal server error
 	response.status(500);
 	response.render("error-500", {
+		mensaje: error.message,
+		pila: error.stack
+	});
+})
+
+app.use((error, request, response, next) =>{
+	//codigo 500: Internal server error
+	response.status(404);
+	response.render("error-404", {
 		mensaje: error.message,
 		pila: error.stack
 	});
