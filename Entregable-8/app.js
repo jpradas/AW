383b763f323b44fb5d400a1c96a2da26c254c24d
Espartaco99 <jperucha@ucm.es>
2018-01-08 02:13:26 +0100

    Casi terminado entregable 8 Falta cambiar los prepend por una solucion optima para que funcione todo adecuadamente

---
+++ Entregable-8/app.js
@@ -0,0 +1,79 @@
+const express = require("express");
+const path = require("path");
+const config = require("./config");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+let idCounter = 5;
+
+let tasks = [
+    {
+        id: 1,
+        text: "Preparar práctica PDAP",
+    },
+    {
+        id: 2,
+        text: "Mirar fechas congreso",
+    },
+    {
+        id: 3,
+        text: "Ir al supermercado",
+    },
+    {
+        id: 4,
+        text: "Mudanza",
+    }
+];
+
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use(bodyParser.json());
+
+app.get("/", (request, response) => {
+    response.redirect("/tasks.html");
+});
+
+app.get("/tasks", (request, response) => {
+  response.status(200);
+  response.json(tasks);
+});
+
+app.post("/tasks", (request, response) => {
+    let texto = request.body.text;
+    let newTask = {
+        id: idCounter++,
+        text: texto,
+    };
+    tasks.push(newTask);
+    response.status(200);
+    console.log(tasks);
+    response.json(newTask);
+});
+
+app.delete("/tasks/:id", (request, response) => {
+    let id = Number(request.params.id);
+    id = id - 1;
+    if (!isNaN(id) && tasks[id] !== undefined) {
+      tasks.splice(id, 1);
+      // Código 200 = OK
+      response.status(200);
+    } else {
+      // Error 404 = Not found
+      response.status(404);
+    }
+    response.json({});
+});
+
+app.listen(config.port, function(err) {
+    if (err) {
+        console.log("No se ha podido iniciar el servidor.")
+        console.log(err);
+    } else {
+        console.log(`Servidor escuchando en puerto ${config.port}.`);
+    }
+});
---
+++ Entregable-8/public/js/index.js
@@ -0,0 +1,107 @@
+"use strict";
+
+/*
+ * Manejador que se ejecuta cuando el DOM se haya cargado.
+ */
+$(() => {
+    // Solicita al servidor la lista de tareas, y la muestra en la página
+    loadTasks();
+
+    // Cuando se pulse alguno de los botones 'Eliminar', se llamará a
+    // la función onRemoveButtonClick
+    $("div.tasks").on("click", "button.remove", onRemoveButtonClick);
+
+    // Cuando se pulse el botón de 'Añadir', se llamará a la función
+    // onAddButtonClick
+    $("button.add").on("click", onAddButtonClick);
+});
+
+/*
+ * Convierte una tarea en un elemento DOM.
+ *
+ * @param {task} Tarea a convertir. Debe ser un objeto con dos atributos: id y text.
+ * @return Selección jQuery con el elemento del DOM creado
+ */
+function taskToDOMElement(task) {
+    let result = $("<li>").addClass("task");
+    result.append($("<span>").text(task.text));
+    result.append($("<button>").addClass("remove").text("Eliminar"));
+    result.data("id", task.id);
+    return result;
+}
+
+function loadTasks() {
+  $.ajax({
+    //method: "GET",
+    type: "GET",
+    url: "/tasks/",
+    // En caso de éxito, colocamos el texto con el resultado
+    // en el documento HTML
+    success: function (data, textStatus, jqXHR) {
+      console.log(textStatus);
+      console.log(data);
+      data.forEach(task => {
+        //Corregir el prepend por algo mejor, before no funciona
+        $("div.tasks > ul").prepend(taskToDOMElement(task));
+      });
+    },
+    // En caso de error, mostramos el error producido
+    error: function (jqXHR, textStatus, errorThrown) {
+      alert("Se ha producido un error: " + errorThrown);
+    }
+    });
+
+}
+
+function onRemoveButtonClick(event) {
+    // Obtenemos el botón 'Eliminar' sobre el que se ha
+    // hecho clic.
+    let selected = $(event.target);
+
+    // Obtenemos el elemento <li> que contiene el botón
+    // pulsado.
+    let liPadre = selected.parent();
+    let id = liPadre.data("id");
+    console.log(id);
+    // Implementar el resto del método aquí.
+    // ...
+    $.ajax({
+      //method: "GET",
+      type: "DELETE",
+      url: "/tasks/" + id,
+      // En caso de éxito, colocamos el texto con el resultado
+      // en el documento HTML
+      success: function (data, textStatus, jqXHR) {
+        //Funciona pero como tengo prepend id -1 no coge el que queremos
+        let tareas = $("div.tasks > ul").children().eq(id - 1);
+        tareas.hide();
+      },
+      // En caso de error, mostramos el error producido
+      error: function (jqXHR, textStatus, errorThrown) {
+        alert("Se ha producido un error: " + errorThrown);
+      }
+      });
+}
+
+function onAddButtonClick(event) {
+  // Obtenemos el valor contenido en el cuadro de texto
+  var valor = $("#taskText").val();
+  console.log(valor);
+  // Realizamos la petición al servidor
+  $.ajax({
+  type: "POST",
+  url: "/tasks/",
+  contentType: "application/json",
+  data: JSON.stringify({ text: valor }),
+  // En caso de éxito, colocamos el texto con el resultado
+  // en el documento HTML
+  success: function (data, textStatus, jqXHR) {
+    console.log(textStatus);
+    $("div.tasks > ul").prepend(taskToDOMElement(data));
+  },
+  // En caso de error, mostramos el error producido
+  error: function (jqXHR, textStatus, errorThrown) {
+    alert("Se ha producido un error: " + errorThrown);
+  }
+  });
+}
